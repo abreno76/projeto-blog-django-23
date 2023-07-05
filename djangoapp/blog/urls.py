515ab6692807a937ac8e8cd6a2aa024871b102d4
Alex Breno <a_breno@hotmail.com>
2023-07-05 16:43:05 -0300

    Migrando a view category para ListView e usando allow_empty para 404

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -1,5 +1,5 @@
-from blog.views import (CreatedByListView, PostListView, category, page, post,
-                        search, tag)
+from blog.views import (CategoryListView, CreatedByListView, PostListView,
+                        page, post, search, tag)
 from django.contrib import admin
 from django.urls import path
 
@@ -10,7 +10,7 @@ urlpatterns = [
     path('post/<slug:slug>/', post, name='post'),
     path('page/<slug:slug>/', page, name='page'),
     path('created_by/<int:author_pk>/', CreatedByListView.as_view(), name='created_by'),
-    path('category/<slug:slug>/', category, name='category'),    
+    path('category/<slug:slug>/', CategoryListView.as_view(), name='category'),    
     path('tag/<slug:slug>/', tag, name='tag'),    
     path('search/', search, name='search'),    
 ]
