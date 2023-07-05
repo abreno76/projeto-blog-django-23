70a06424edcb91f046eef0a6b2e63a63390f3bd3
Alex Breno <a_breno@hotmail.com>
2023-07-05 16:59:26 -0300

    Migrando a view tag para Class Based View (CBV) ListView no views.py do Django

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -93,50 +93,24 @@ class CategoryListView(PostListView):
         return ctx
 
 
+class TagListView(PostListView):
+    allow_empty = False
 
-def category(request, slug):
-    posts = Post.objects.get_published()\
-        .filter(category__slug=slug)
-
-    paginator = Paginator(posts, PER_PAGE)
-    page_number = request.GET.get("page")
-    page_obj = paginator.get_page(page_number)
-
-    if len(page_obj) == 0:
-        raise Http404()
-
-    page_title = f'{page_obj[0].category.name} - Categoria - '
-
-    return render(
-        request,
-        'blog/pages/index.html',
-        {
-            'page_obj': page_obj,
+    def get_queryset(self):
+        return super().get_queryset().filter(
+            tags__slug=self.kwargs.get('slug')
+        )
+    
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        page_title = (
+            f'{self.object_list[0].tags.first().name}'  # type: ignore
+            ' - Tag - '
+        )
+        ctx.update({
             'page_title': page_title,
-        }
-    )
-
-def tag(request, slug):
-    posts = Post.objects.get_published()\
-        .filter(tags__slug=slug)
-
-    paginator = Paginator(posts, PER_PAGE)
-    page_number = request.GET.get("page")
-    page_obj = paginator.get_page(page_number)
-
-    if len(page_obj) == 0:
-        raise Http404()
-
-    page_title = f'{page_obj[0].tags.first().name} - Tag - '
-
-    return render(
-        request,
-        'blog/pages/index.html',
-        {
-            'page_obj': page_obj,
-            'page_title': page_title,
-        }
-    )
+        })
+        return ctx
 
 
 def search(request):
@@ -162,7 +136,6 @@ def search(request):
             'page_title': page_title,
         }
     )
-
 
 
 def page(request, slug):
